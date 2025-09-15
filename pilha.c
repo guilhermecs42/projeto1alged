d924bb6b9543665d3bdcc80bfea1b3d1c893302f
Guilherme Cavalcanti de Santana <90415833+guilhermecs42@users.noreply.github.com>
2025-09-15 09:45:04 -0300

    Update and rename pilhadinamica.c to pilha.c
    criei as funções imprimir e buscar que estavam faltando. A função buscar ela só diz se um item está na pilha ou não usando a função item_comparar, pois o princípio LIFO impede que o usuário manipule algo que não está no topo da pilha. Agora a pilha usa a TabelaFuncoes que o TAD tratamento usa.

--- pilha.c
+++ pilha.c
@@ -1,34 +1,30 @@
 #include<stdlib.h>
-#include<stdbool.h>
 #include<string.h>
-#include<stdio.h>
-#include "pilhadinamica.h"
+#include "pilha.h"
+#include "tabela_funcoes.h"
 
-typedef struct no NO;
+typedef struct pilha_ PILHA;
+typedef struct no_ NO;
 
-struct no{
+struct no_{
 	void* item; // item que este nó contém
 	NO* anterior; // ponteiro para o nó anterior
 };
 
-struct pilha{
+struct pilha_{
 	NO* topo; // é um ponteiro para o elemento no topo da pilha
 	int altura;
-	void (*apagar_elemento)(void* elemento);
-	void (*salvar_elemento)(void* elemento, FILE* arquivo);
-	bool (*carregar_elemento)(void** elemento, FILE* arquivo);
+	const TabelaFuncoes* item_funcoes;
 };
 
 
 
-PILHA* pilha_criar(void (*item_apagar)(void* item), void (*item_salvar)(void* item, FILE* arquivo), bool (*item_carregar)(void** item, FILE* arquivo)){
+PILHA* pilha_criar(const TabelaFuncoes* funcoes){
 	PILHA* pilha = (PILHA*)malloc(sizeof(PILHA));
 	if(pilha != NULL){
 		pilha->topo = NULL;
 		pilha->altura = 0;
-		pilha->apagar_elemento = item_apagar;
-		pilha->salvar_elemento = item_salvar;
-		pilha->carregar_elemento = item_carregar;
+		pilha->item_funcoes = funcoes;
 	}
 	return pilha;
 }
@@ -45,6 +41,7 @@ bool pilha_cheia(PILHA* pilha){ // verifica se há espaço disponível na heap
 		NO* novo = (NO*)malloc(sizeof(NO)); // tenta alocar espaço para mais um nó na heap
 		if(novo != NULL){ // se a alocação der certo:
 			free(novo); // desaloco o nó porque não vou usá-lo
+			novo = NULL;
 			return false; // como pude alocar o nó, a heap ainda não está cheia
 		}
 		return true; // se a alocação der errado, a heap está cheia
@@ -90,12 +87,12 @@ void* pilha_desempilhar(PILHA* pilha){
 	return NULL;
 }
 
-void pilha_apagar(PILHA** pilha_ptr){
+bool pilha_apagar(PILHA** pilha_ptr){
 	NO* no_aux;
 	if(*pilha_ptr != NULL && !pilha_vazia(*pilha_ptr)){
 		while((*pilha_ptr)->topo != NULL){
 			no_aux = (*pilha_ptr)->topo;
-			(*pilha_ptr)->apagar_elemento(no_aux->item);
+			(*pilha_ptr)->item_funcoes->item_apagar(&(no_aux->item));
 			(*pilha_ptr)->topo = no_aux->anterior;
 			free(no_aux);
 		}
@@ -104,12 +101,40 @@ void pilha_apagar(PILHA** pilha_ptr){
 	*pilha_ptr = NULL;
 }
 
+
+bool pilha_imprimir(PILHA* pilha){
+	if(pilha!=NULL){
+		NO* no_aux = pilha->topo;
+		while(no_aux!=NULL){
+			if(pilha->item_funcoes->item_imprimir(no_aux->item) == false){ // chama a função imprimir_elemento e se ela falhar, retorna false
+				return false;
+			}
+			no_aux = no_aux->anterior;
+		}
+		return true;
+	}
+	return false;
+}
+
+bool pilha_buscar(PILHA* pilha, void* item){ // uma função que verifica se um determinado item já está na pilha
+	if(pilha != NULL && item != NULL){
+		NO* no_aux = pilha->topo;
+		while(no_aux != NULL){
+			if(pilha->item_funcoes->item_comparar(no_aux->item, item) == 0){ // faz a verificação e se forem iguais:
+				return true;
+			}
+		}
+		return false;
+	}
+	return false;
+}
+
 void pilha_salvar_aux(PILHA* pilha, FILE* arquivo, NO* no_atual){
 	if(no_atual == NULL){ // se eu passei da base da pilha, volta
 		return;
 	}
 	pilha_salvar_aux(pilha, arquivo, no_atual->anterior); // chama a função para o próximo elemento da pilha
-	pilha->salvar_elemento(no_atual->item, arquivo); // essa linha só roda depois de chegar na base da pilha pela linha anterior, a partir daí vai voltando de chamada em chamada salvando cada elemento até o topo
+	pilha->item_funcoes->item_salvar(no_atual->item, arquivo); // essa linha só roda depois de chegar na base da pilha pela linha anterior, a partir daí vai voltando de chamada em chamada salvando cada elemento até o topo
 	return;
 }
 
@@ -124,29 +149,31 @@ bool pilha_salvar(PILHA* pilha, FILE* arquivo){
 }
 
 bool pilha_carregar(PILHA** pilha, FILE* arquivo){
-	if(*pilha == NULL || (*pilha)->carregar_elemento == NULL){
+	if(*pilha == NULL || (*pilha)->item_funcoes->item_carregar == NULL){
 		return false;
 	}
 	char linha[100];
-	PILHA* pilha_temp = pilha_criar((*pilha)->apagar_elemento, (*pilha)->salvar_elemento, (*pilha)->carregar_elemento);
+	long int ultima_pos;
+	ultima_pos = ftell(arquivo);
 	fgets(linha, 100, arquivo); // lê o cabeçalho
 	if(strcmp(linha, "======BASE=PILHA======\n")!=0){ // se não for exatamente a linha esperada, dá erro
-		pilha_apagar(&pilha_temp); // apagar a pilha temporária
+		fseek(arquivo, ultima_pos, SEEK_SET); // retorna à posição antes do erro
 		return false; // retorna erro
 	}
+	PILHA* pilha_temp = pilha_criar((*pilha)->item_funcoes); // cria uma pilha temporária com as mesmas funções da pilha recebida
 	while(1){
 		void* item; // cria uma variável local que vai receber o endereço do próximo item a ser inserido na pilha
-		if(pilha_temp->carregar_elemento(&item, arquivo) == false){
+		if(pilha_temp->item_funcoes->item_carregar(&item, arquivo) == false){
 			// lê do arquivo o próximo item da pilha e guarda o endereço da struct na variável local.
 			// se falhar, sai do loop: pode ser que tenha chegado no rodapé, ou que o arquivo esteja corrompido
-			// quando a função carregar_elemento falha, o cursor deve estar no começo da linha que gerou o erro.
+			// quando a função item_carregar falha, o cursor deve estar no começo da linha que gerou o erro
 			break;
 		} 
 		pilha_empilhar(pilha_temp, item); // coloca na pilha temporária
 	}
 	fgets(linha, 100, arquivo); // lê a linha que parou o loop
 	if(strcmp(linha, "======TOPO=PILHA======\n") != 0){ // se não for exatamente o rodapé, dá erro
-		pilha_apagar(&pilha_temp);
+		pilha_apagar(&pilha_temp); // apaga a pilha temporária e todos os itens dela
 		return false;
 	}
 	// caso dê certo:
