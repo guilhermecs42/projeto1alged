7c4804d011b3a5606168d50c7cc7d656b26a481b
CleyJosRM <cleyjoserm2@gmail.com>
2025-10-04 22:06:27 -0300

    Update fila.c

--- fila.c
+++ fila.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include "fila.h"
+#include <string.h>
+#include "tabela_funcoes.h"
 
 typedef struct no_ NO;
+typedef struct fila_ FILA;
+
 struct no_{
     void* item;
     NO* prox;
@@ -13,19 +16,17 @@ struct fila_{
     NO* inicio;
     NO* fim;
     int tam;
+    const TabelaFuncoes* item_funcoes;
 };
 
-FILA *fila_criar(void){
-
-    FILA* f;
-    f = (FILA*)malloc(sizeof(FILA));
-
+FILA* fila_criar(const TabelaFuncoes* funcoes){
+    FILA* f = (FILA*)malloc(sizeof(FILA));
     if(f != NULL){
         f->inicio = NULL;
         f->fim = NULL;
         f->tam = 0;
+        f->item_funcoes = funcoes;
     }
-
     return f;
 }
 
@@ -40,7 +41,6 @@ bool fila_vazia(FILA* fila){
     return (fila->tam == 0);
 }
 
-//reciclei do pilhadinamica.c
 bool fila_cheia(FILA* fila){
     if(fila != NULL){
 		NO* novo = (NO*)malloc(sizeof(NO)); // tenta alocar espaço para mais um nó na heap
@@ -53,7 +53,6 @@ bool fila_cheia(FILA* fila){
 	return true;
 }
 
-// Retorna o primeiro item da fila, sem removê-lo
 void* fila_frente(FILA* fila){
     if(!fila_vazia(fila)){
         return fila->inicio->item;
@@ -62,24 +61,18 @@ void* fila_frente(FILA* fila){
 }
 
 bool fila_inserir(FILA* fila, void* item){
-
     if(!fila_cheia(fila)){
-
         NO* n = (NO*)malloc(sizeof(NO));
-
         if(n != NULL){
             n->item = item;
             n->prox = NULL;
-
             if(fila_vazia(fila)){     
                 fila->inicio = n;    // Se a fila está vazia, o novo nó é também o início da fila
             }else{
                 fila->fim->prox = n; // Se não, o antigo fim aponta para o novo nó
             }
-
             fila->fim = n;            // O novo nó é sempre o novo fim da fila
-            fila->tam++;
-            
+            fila->tam++;       
             return true;
         }
     }
@@ -87,17 +80,12 @@ bool fila_inserir(FILA* fila, void* item){
 }
 
 void* fila_remover(FILA* fila){
-    
     if(!fila_vazia(fila)){
-
         void* item = fila->inicio->item;  // O ponteiro auxiliar 'item' recebe o endereço do item armazenado na fila
-
         NO* prox = fila->inicio->prox;    // O endereço do próximo nó após o início é armazenado num auxiliar 'prox'
         free(fila->inicio);               // A memória alocada pro nó de início é liberada e
         fila->inicio = prox;              // o início da fila recebe o endereço armazenado no auxiliar 'prox'
-        
         fila->tam--;
-
         if(fila->tam == 0){               // Caso depois da remoção a fila fique vazia,
             fila->fim = NULL;             // o fim da fila também deve receber NULL
         }
@@ -107,64 +95,88 @@ void* fila_remover(FILA* fila){
 }
 
 void fila_apagar(FILA** fila){
-
-    if(fila !=  NULL && *fila != NULL){
-
+    if(fila != NULL && *fila != NULL){
         NO* no_atual = (*fila)->inicio;
         NO* no_aux;
 
         while (no_atual != NULL) {
-            no_aux = no_atual->prox; // Guarda o próximo nó
-            free(no_atual);          // Libera o nó atual
-            no_atual = no_aux;       // Avança para o próximo
+            no_aux = no_atual->prox;     // Guarda o próximo nó
+            if ((*fila)->item_funcoes != NULL && (*fila)->item_funcoes->item_apagar != NULL) {
+                (*fila)->item_funcoes->item_apagar(&(no_atual->item));
+            }
+            free(no_atual);              // Libera o nó atual
+            no_atual = no_aux;           // Avança para o próximo
         }
-
         free(*fila);
         *fila = NULL; 
     }
-    return;
 }
 
-// Imprime a fila usando um ponteiro para a função que imprimi cada item genérico
-void fila_imprimir(FILA* fila, void (*funcao_imprimir)(void*)){
-
-    if (fila_vazia(fila)) {
-        printf("Fila vazia ou nula.\n");
+void fila_imprimir(FILA* fila){
+    if (fila == NULL || fila_vazia(fila) || fila->item_funcoes == NULL || fila->item_funcoes->item_imprimir == NULL) {
         return;
     }
-
     NO* no_atual = fila->inicio;
-
-    printf("Fila: [INÍCIO]\n");
+    printf("======INICIO=FILA======\n");
     while (no_atual != NULL) {
-        funcao_imprimir(no_atual->item);  // Passa o ponteiro pro item do nó atual como argumento para a função que o imprimirá
+        fila->item_funcoes->item_imprimir(no_atual->item); // Chamada via tabela de funções
         no_atual = no_atual->prox;
     }
-    printf("[FIM]\n");
+    printf("======FIM=FILA======\n");
 }
 
-void fila_salvar(FILA* fila, char* nomearquivo, void (*funcao_salvar)(void*, FILE*)){
-    if (fila_vazia(fila)) {
-        printf("Fila vazia ou nula.\n");
-        return;
+bool fila_salvar(FILA* fila, FILE* arquivo){
+    if (fila == NULL || arquivo == NULL || fila->item_funcoes == NULL || fila->item_funcoes->item_salvar == NULL) {
+        return false;
     }
 
-    FILE* arquivo;
-    arquivo = fopen(nomearquivo, "w");
-
-    if(arquivo == NULL){
-        printf("Erro ao abrir arquivo.\n");
-        return;
-    }
-
+    fprintf(arquivo, "======INICIO=FILA======\n");
     NO* no_atual = fila->inicio;
-
-    fprintf(arquivo, "Fila: [INÍCIO]\n");
     while (no_atual != NULL) {
-        funcao_salvar(no_atual->item, arquivo);
+        fila->item_funcoes->item_salvar(no_atual->item, arquivo); // Chamada via tabela
         no_atual = no_atual->prox;
     }
+    fprintf(arquivo, "======FIM=FILA======\n");
+    return true;
+}
 
-    fprintf(arquivo, "[FIM]\n");
-    fclose(arquivo);
+bool fila_carregar(FILA** fila, FILE* arquivo){
+    if(*fila == NULL || (*fila)->item_funcoes == NULL || (*fila)->item_funcoes->item_carregar == NULL){
+		return false;
+	}
+	char linha[100];
+    long int ultima_pos = ftell(arquivo);
+	fgets(linha, 100, arquivo);                          // lê o cabeçalho
+	if(strcmp(linha, "======INICIO=FILA======\n") != 0){ // se não for exatamente a linha esperada, dá erro
+        fseek(arquivo, ultima_pos, SEEK_SET);
+		return false;
+	}
+
+        // Cria fila temporária com as mesmas funções da original
+	FILA* fila_temp = fila_criar((*fila)->item_funcoes);
+    if(fila_temp == NULL){
+        fseek(arquivo, ultima_pos, SEEK_SET);
+        return false;
+    }
+
+	while(1){
+		void* item;
+		if(fila_temp->item_funcoes->item_carregar(&item, arquivo) == false){
+            // lê do arquivo o próximo item da fila e guarda o endereço da struct na variável local.
+			// se falhar, sai do loop: pode ser que tenha chegado no rodapé, ou que o arquivo esteja corrompido
+			// quando a função carregar_elemento falha, o cursor deve estar no começo da linha que gerou o erro.
+			break;
+		} 
+		fila_inserir(fila_temp, item); // coloca na fila temporária
+	}
+
+	fgets(linha, 100, arquivo);                        // lê a linha que parou o loop
+	if(strcmp(linha, "======FIM=FILA======\n") != 0){  // se não for exatamente o rodapé, dá erro
+		fila_apagar(&fila_temp);
+		return false;
+	}
+	// caso dê certo:
+	fila_apagar(fila); // apaga a fila antiga
+	*fila = fila_temp; // substitui pela fila nova
+	return true;
 }
