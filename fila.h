97d5adadf96116e69be1f3baf426aa663b6bd9b2
CleyJosRM <cleyjoserm2@gmail.com>
2025-10-04 22:06:57 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -1,11 +1,12 @@
 #ifndef FILA_H
 	#define FILA_H
-	#include<stdio.h>
-	#include<stdbool.h>
+	#include <stdio.h>
+	#include <stdbool.h>
+    #include "tabela_funcoes.h"
 	
 	typedef struct fila_ FILA;
 	
-	FILA *fila_criar(void);
+	FILA *fila_criar(const TabelaFuncoes* funcoes);
 	int fila_tamanho(FILA* fila);
 	bool fila_vazia(FILA* fila);
 	bool fila_cheia(FILA* fila);
@@ -13,7 +14,8 @@
 	bool fila_inserir(FILA* fila, void* item);
 	void* fila_remover(FILA* fila);
 	void fila_apagar(FILA** fila);
-	void fila_imprimir(FILA* fila, void (*funcao_imprimir)(void*));
-	void fila_salvar(FILA* fila, char* nomearquivo, void (*funcao_salvar)(void*, FILE*));
+	void fila_imprimir(FILA* fila);
+	bool fila_salvar(FILA* fila, FILE* arquivo);
+	bool fila_carregar(FILA** fila, FILE* arquivo);
 	
 #endif
