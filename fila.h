3d007a72c7b27b822e18ff110d5ec590a0691b6b
Guilherme Cavalcanti de Santana <90415833+guilhermecs42@users.noreply.github.com>
2025-10-09 00:17:25 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -12,7 +12,7 @@
 	bool fila_cheia(FILA* fila);
 	void* fila_frente(FILA* fila);
 	bool fila_inserir(FILA* fila, void* item);
-	void* fila_remover(FILA* fila);
+	void* fila_remover_frente(FILA* fila);
 	void fila_apagar(FILA** fila);
 	void fila_imprimir(FILA* fila);
 	bool fila_salvar(FILA* fila, FILE* arquivo);
