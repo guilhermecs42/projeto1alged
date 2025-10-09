4f303c6710a6171122c9a54ff21d1050d86ba87a
Guilherme Cavalcanti de Santana <90415833+guilhermecs42@users.noreply.github.com>
2025-10-09 00:22:15 -0300

    Update tratamento.c

--- tratamento.c
+++ tratamento.c
@@ -7,8 +7,6 @@ struct tratamento_{
 	char medicamento[101];
 };
 
-
-
 TRATAMENTO* tratamento_criar(const char medicamento[]){
 	if(strlen(medicamento)>100){ // se a string de input tiver mais de 99 caracteres, não dá para criar esse TAD
 		return NULL;
@@ -23,7 +21,7 @@ TRATAMENTO* tratamento_criar(const char medicamento[]){
 
 
 bool tratamento_apagar(void** tratamento){
-	if(tratamento == NULL){
+	if(tratamento == NULL || *tratamento == NULL){
 		return false;
 	}
 	TRATAMENTO** t = (TRATAMENTO**) tratamento;
@@ -46,6 +44,10 @@ bool tratamento_imprimir(void* tratamento){
 
 
 int tratamento_comparar(void* tratamento1, void* tratamento2){
+	if(tratamento1 == NULL && tratamento2 == NULL) // se ambos forem nulos, são considerados iguais
+		return 0;
+	if(tratamento2 == NULL) return 1; // se o segundo for nulo e o primeiro não, retorna positivo
+	if(tratamento1 == NULL) return -1; // se o primeiro for nulo e o segundo não, retorna negativo
 	TRATAMENTO* t1 = (TRATAMENTO*) tratamento1;
 	TRATAMENTO* t2 = (TRATAMENTO*) tratamento2;
 	
@@ -69,23 +71,29 @@ bool tratamento_carregar(void** tratamento, FILE* arquivo){ // aqui o "tratament
 	if(tratamento == NULL || arquivo == NULL){
 		return false;
 	}
-	char buffer[101];
+	char buffer[151];
 	char medicamento[101];
-	long int posicao_salva = ftell(arquivo); // é necessário salvar a posição do cursor no arquivo, caso dê errado é necessário retornar para essa posição
-	fgets(buffer, 101, arquivo); // lendo o cabeçalho
-	
+	long int posicao_salva = ftell(arquivo); // é necessário salvar a posição do cursor no arquivo NO INÍCIO DO REGISTRO, caso dê errado é necessário retornar para essa posição
+	if(fgets(buffer, 151, arquivo)==NULL){ // lendo o cabeçalho
+		fseek(arquivo, posicao_salva, SEEK_SET); // o cursor fica no começo da linha que gerou o erro
+		return false;
+	}
 	if(strcmp(buffer, "===INICIO==TRATAMENTO===\n")==0){ // se o cabeçalho está correto
 	
-		fgets(medicamento, 101, arquivo); // lendo o medicamento salvo, incluindo \n
+		if(fgets(medicamento, 101, arquivo)==NULL){ // lendo o medicamento salvo, incluindo \n
+			fseek(arquivo, posicao_salva, SEEK_SET); // o cursor fica no começo da linha que gerou o erro
+			return false; 
+		}
 		medicamento[strcspn(medicamento, "\n")] = '\0'; // eliminando o \n 
-		posicao_salva = ftell(arquivo); // salvando a posição no início do rodapé, antes de lê-lo
-		fgets(buffer, 101, arquivo); // lendo o rodapé
+		if(fgets(buffer, 151, arquivo)==NULL){ // lendo o rodapé
+			fseek(arquivo, posicao_salva, SEEK_SET); // o cursor fica no começo da linha que gerou o erro
+			return false; 
+		}
 		
 		if(strcmp(buffer, "=====FIM=TRATAMENTO=====\n")==0){ // se o rodapé está correto
 			
-			TRATAMENTO** t = (TRATAMENTO**) tratamento; // converte o ponteiro void** para TRATAMENTO**
-			*t = tratamento_criar(medicamento); // cria um tratamento e coloca o medicamento lido nele. Então, modifica a variável original para receber o endereço desse medicamento
-			if(*t == NULL){ // se tratamento_criar retornou nulo, significa que não conseguiu alocar
+			*tratamento = (void*)tratamento_criar(medicamento); // cria um tratamento e coloca o medicamento lido nele. Então, modifica a variável original para receber o endereço desse medicamento
+			if(*tratamento == NULL){ // se tratamento_criar retornou nulo, significa que não conseguiu alocar
 				// acho que aqui não devemos alterar a posição do cursor, pois o erro não foi na leitura de alguma linha, mas sim na alocação de memória
 				return false;
 			}
@@ -100,6 +108,12 @@ bool tratamento_carregar(void** tratamento, FILE* arquivo){ // aqui o "tratament
 	return false;
 }
 
+void* tratamento_copiar(void* tratamento){
+	if(tratamento == NULL) return NULL;
+	TRATAMENTO* copia = tratamento_criar(((TRATAMENTO*)tratamento)->medicamento);
+	return copia;
+}
+
 
 
 static const TabelaFuncoes TratamentoFuncoes = { // estou criando uma struct do tipo TabelaFuncoes e fazendo cada atribuição
@@ -107,9 +121,10 @@ static const TabelaFuncoes TratamentoFuncoes = { // estou criando uma struct do 
 	.item_imprimir = tratamento_imprimir,
 	.item_comparar = tratamento_comparar,
 	.item_salvar = tratamento_salvar,
-	.item_carregar = tratamento_carregar
+	.item_carregar = tratamento_carregar,
+	.item_copiar = tratamento_copiar
 };
 
 const TabelaFuncoes* get_tratamento_funcoes(){
-	return &TratamentoFuncoes;
+	return &TratamentoFuncoes; // como a variável é global, ela sempre existe
 }
